75b678d7a74869ba52407c63c01cb2d151b98897
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2025-02-17 22:18:55 +0600

    navbar design done

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,55 @@
+import Image from "next/image"
+import logo from '@/assets/logo.svg'
+import { Button } from "@/components/ui/button"
+export default function NavBar() {
+    const links = <>
+        <li><a>Home</a></li>
+        <li><a>About</a></li>
+        <li><a>Services</a></li>
+        <li><a>Blog</a></li>
+        <li><a>Contact</a></li>
+    </>
+    return (
+        <nav>
+            <div className="navbar bg-base-100 container mx-auto">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            {
+                                links
+                            }
+                        </ul>
+                    </div>
+                    <Image src={logo} alt="Logo image" />
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+
+                        {
+                            links
+                        }
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <button className="btn btn-outline btn-error">Appoinment</button>
+                </div>
+            </div>
+        </nav>
+    )
+}
